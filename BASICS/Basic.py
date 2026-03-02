7ed93296154732c0919126313e77c90c98993562
leelavathikm1 <leelavathikm1@gmail.com>
2026-03-02 22:34:58 +0530

    data-operators

---
+++ BASICS/Basic.py
@@ -0,0 +1,32 @@
+is_true=True
+
+age=18
+
+#can_vote=age>18
+#print(can_vote)
+
+is_18=age==18
+print(is_18)
+print(age!=12)
+
+#print(2+3)*4
+
+myAge=25
+hasLicense=True
+
+can_Drive=myAge>=16 and hasLicense
+print(can_Drive)    
+
+day="Mondayday"
+is_weekend=day=="Saturday" or day=="Sunday"
+print(is_weekend)   
+
+is_adult=age>=18
+is_child=not is_adult
+print(is_child)
+
+score=10
+
+score=score+5
+score+=5
+print(score)     
--- BASICS/hello.py
+++ BASICS/hello.py
@@ -1,4 +1,42 @@
+import requests
+
 print("Hello Python")
 print("Hello World") 
 print("Hi Leela Welcome to Python AI World")
 print("Today is a Great Day To Start Learning Python")
+response = requests.get("https://api.github.com")
+print(response.json()) 
+
+name="Leela"
+
+age=40
+
+total=10-7
+
+power =10*2
+
+power=10**2
+
+half=10/2
+
+result=10//3
+
+
+string="My Name is Leela"
+
+my_long_name="""my name is Leela and I am learning Python"""
+
+"""my name is Leela and I am learning Python"""
+first_name="Leela"
+last_name="KM"
+full_name=first_name+" "+last_name 
+
+longDash="-"*10
+
+print(longDash)
+print(full_name)
+print(longDash)
+
+len(longDash)
+print(len)
+
