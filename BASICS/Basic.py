ecee93869cab2d16789fcc54f2ace7e4e4cab526
leelavathikm1 <leelavathikm1@gmail.com>
2026-03-03 15:30:43 +0530

    ListMethods

--- BASICS/Basic.py
+++ BASICS/Basic.py
@@ -29,4 +29,53 @@ score=10
 
 score=score+5
 score+=5
-print(score)     
+print(score)    
+
+name="Leela"
+string=f"Hello {name}" 
+print(string)   
+
+myfullname="Leelavathi KM"
+print(myfullname[1])
+
+myfullname.lower()
+print(myfullname.lower())
+print(myfullname.upper())
+
+sentence="Python is great"
+print(sentence.split()) 
+sentence.title()
+print(sentence.title())
+
+#If Condition
+
+temperature=30
+if temperature>25:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature>20:
+    print("It's a nice day")
+elif temperature>15:
+    print("It's a bit cold")
+else:
+    print("It's cold")
+print("Done") 
+
+childage=12
+hasLicense=True
+
+if childage>=18 and hasLicense:
+    print("You can drive")      
+else:
+    print("You cannot drive")
+    
+    hasTicket=True
+    watchAge=15
+    
+    if hasTicket:
+        if watchAge>=18:
+            print("You can watch")
+        else:
+            print("You cannot watch")  
+    else: 
+        print("Please Buy a Ticket") 
---
+++ BASICS/loops.py
@@ -0,0 +1,17 @@
+for i in range(5):
+    print(i) 
+    print("Hello"   )   
+    
+    #count 1 to 5
+    for i in range(1,6):
+        print(i)
+        
+    #count 1 to 10 with step of 2 odd numbers
+    for i in range(1,11,2):
+        print(i)
+        
+    #count 1 to 10 with step of 2 even numbers
+    for i in range(0,10,2):
+        print(i)
+        
+    
