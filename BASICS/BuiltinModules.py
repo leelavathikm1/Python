6d7b55a1956ae7e4fa952557a82155d31bab59ea
leelavathikm1 <leelavathikm1@gmail.com>
2026-03-06 15:25:04 +0530

    analyzer

--- BASICS/BuiltinModules.py
+++ BASICS/BuiltinModules.py
@@ -26,4 +26,48 @@ print(result)
 #print(circle_area)
 
 
+import requests
+response=requests.get("https://api.github.com")
+print(response.json())  
+
+import pandas as pd
+data={"Name": ["Alice", "Bob", "Charlie"], "Age": [25, 30, 35]}
+df=pd.DataFrame(data)
+print(df)   
+
+import requests
+latitude=40.7128
+longitude=-74.0060  
+response=requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true")
+weather_data=response.json()
+print(weather_data) 
+
+
+from datetime import datetime,timedelta
+today=datetime.now()
+week_ago=today-timedelta(days=7)
+start_date=week_ago.strftime("%Y-%m-%d")
+end_date=today.strftime("%Y-%m-%d")
+url=f"https://api.covid19api.com/country/united-states/status/confirmed?from={start_date}T00:00:00Z&to={end_date}T00:00:00Z"
+response=requests.get(url)
+data=response.json()
+print(data)
+
+#-----------------------
+#extract the daily data 
+daily_data=['daily']
+
+#create adataframe
+df=pd.dataFrame({
+    'date':daily_data['time'],
+   ' max_temp':daily_data['temperature_2m_max'],
+    'min_temp':daily_data['temperature_2m_min'],
+    
+})
+
+#convertdate string to datetime
+df['date']=pd.to_datetime(df['date'])   
+print(df)
+
+
 
